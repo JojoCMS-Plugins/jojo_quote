e3bc3f42f6a04545586420aefadfe820c646e8c6
Tom Dale <tom@zero.co.nz>
2014-10-29 20:38:03 +0000

    Quote: remove styling and images

--- global.php
+++ global.php
@@ -26,22 +26,3 @@ if (Jojo::getOption('quote_num_sidebar_articles') >= 1) {
     $quotes = array_slice($quotes, 0, Jojo::getOption('quote_num_sidebar_articles', 1)); 
     $smarty->assign('quotes', $quotes);
 }
-
-/** Example usage in theme template:
-            {if $quotes}
-            <div id="quotebox" class="sidebarbox">
-                <h2>Testimonials</h2>
-                {foreach from=$quotes key=key item=quote}
-                    <h3>{$quote.title}</h3>
-                    <div class="quote">
-                        {if $quote.imageurl}<img class="quoteimage" src="{$quote.imageurl}" alt="{$quote.author}" />{/if}
-                        <div class="quotebody">{$quote.body}</div>
-                        <div class = "quotecredit">{$quote.author}</div>
-                        <p>{$quote.description|truncate:150:"..."}<br />
-                        <a class="links" href="{$quote.url}">&gt; Read more</a></p>
-                     </div>
-                {/foreach}
-                <p class="links"><a href='{$SITEURL}/{if _MULTILANGUAGE}{$lclanguage}/{/if}{$quoteshome}/'>&gt;  See all testimonials</a></p>
-            </div>
-            {/if}
-*/
